f4d2818bb704ae20e88626316077e3ef64bbc7b7
kemal.demirtas <kemal.demirtas@beamonpeople.se>
2020-02-19 13:57:45 +0100

    css actually works

--- index.js
+++ index.js
@@ -2,15 +2,18 @@
 const mongoose = require('mongoose')
 const path = require('path')
 const sassMiddleware = require('node-sass-middleware')
-
-const { app, port } = require('./src/server')
+const { app, port, express } = require('./src/server')
 const dbConfig = require('./config/config')
 
 app.use(sassMiddleware({
     src: path.join(__dirname, 'sass'),
-    dest: path.join(__dirname, 'public')
+    dest: path.join(__dirname, '/'),
+    debug: true,
+    outputStyle: 'compressed'
 }))
-app.set('view engine', 'ejs')
+
+// define a static folder, 'public'
+app.use(express.static('public'))
 
 // Kicka igång servern
 const dbOptions = { useUnifiedTopology: true, useNewUrlParser: true }
--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 // Här ska vi definiera vår server! Men vi ska inte starta den
 // (alltså app.listen(port, ()=>{}), har vi inte i denna fil, utan i index.js)
 const express = require('express')
+const sassMiddleware = require('node-sass-middleware')
+const path = require('path')
 const app = express()
 const port = 8080
 
-// define a static folder, 'public'
-app.use('/public', express.static('public'))
+
+
+// define what view engine to use, ejs in this case
+app.set('view engine', 'ejs')
 
 // Route till /
 app.get('/', (req, res) => {
@@ -13,6 +17,4 @@ app.get('/', (req, res) => {
     res.status(200).render('gallery', { enGrej })
 })
 
-
-
-module.exports = { app, port }
+module.exports = { app, port, express }
