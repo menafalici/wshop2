8ac5ac9abd6627fc7fa04d6c2947723b5347f680
kemal.demirtas <kemal.demirtas@beamonpeople.se>
2020-02-19 11:18:09 +0100

    Mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Härifrån startar vi upp vår webshop
+const mongoose = require('mongoose')
 const { app, port } = require('./src/server')
+const dbConfig = require('./config/config')
 
 
 // Kicka igång servern
-const server = app.listen(port, () => console.log(`App listening on port ${port}!`))
+const dbOptions = { useUnifiedTopology: true, useNewUrlParser: true }
+mongoose.connect(dbConfig.databaseURL, dbOptions).then(() => {
+    app.listen(port, () => console.log(`App listening on port ${port}!`))
+})
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 // Här ska vi definiera vår server! Men vi ska inte starta den
 // (alltså app.listen(port, ()=>{}), har vi inte i denna fil, utan i index.js)
-
 const express = require('express')
+
 const app = express()
 const port = 8080
 
