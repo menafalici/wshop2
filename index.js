4ec83f6653b42a03920102c24e81346e5b9dc311
kemal.demirtas <kemal.demirtas@beamonpeople.se>
2020-02-19 13:11:33 +0100

    sass/css connected

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // Härifrån startar vi upp vår webshop
 const mongoose = require('mongoose')
+const path = require('path')
+const sassMiddleware = require('node-sass-middleware')
+
 const { app, port } = require('./src/server')
 const dbConfig = require('./config/config')
 
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public')
+}))
+app.set('view engine', 'ejs')
 
 // Kicka igång servern
 const dbOptions = { useUnifiedTopology: true, useNewUrlParser: true }
--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 // Här ska vi definiera vår server! Men vi ska inte starta den
 // (alltså app.listen(port, ()=>{}), har vi inte i denna fil, utan i index.js)
 const express = require('express')
-
 const app = express()
 const port = 8080
 
-// Route till /
-app.get('/', (req, res) => res.send('Hello World!'))
+// define a static folder, 'public'
+app.use('/public', express.static('public'))
 
+// Route till /
+app.get('/', (req, res) => {
+    const enGrej = 'Hallå en grej'
+    res.status(200).render('gallery', { enGrej })
+})
 
 
 
