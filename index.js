8b0b90f34ab01e6d7dc1373347afcf26a50af5c4
Menaf Alici <alicimenaf@medieinstitutet.se>
2020-02-28 13:08:09 +0100

    removed local config file require2

--- index.js
+++ index.js
@@ -8,10 +8,18 @@ const dbOptions = { useUnifiedTopology: true, useNewUrlParser: true }
 
 let dbUrl = process.env.MONGO_ATLAS_URL
 
+if (dbUrl == undefined) {
+    try {
+        dbUrl = require('./config/config').databaseURL
+    } catch (exception) {
+        console.log("could not load local config file", exception.message)
+    }
+}
 
 
 
-mongoose.connect(dbConfig.databaseURL, dbOptions).then(() => {
+
+mongoose.connect(dbUrl, dbOptions).then(() => {
     app.listen(port, () => console.log(`App listening on port ${port}!`))
 })
 
