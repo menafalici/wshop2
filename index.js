213c2033da6ac148cc52301606dbc8d70f3d6a34
Menaf Alici <menafalici@Menafs-Air.mediegruppen.se>
2020-02-28 09:33:19 +0100

    try-catch

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 // Härifrån startar vi upp vår webshop
-const mongoose = require('mongoose')
 
-const { app, port } = require('./src/server')
-const dbConfig = require('./config/config')
+
 
 
 // Kicka igång servern
+const mongoose = require('mongoose')
+const { app, port } = require('./src/server')
+const dbConfig = require('./config/config')
 const dbOptions = { useUnifiedTopology: true, useNewUrlParser: true }
+
+let dbUrl = process.env.MONGO_ATLAS_URL
+
+if (dbUrl == undefined) {
+
+    try {
+        dbUrl = require('./config/config').databaseURL
+    } catch (exception) {
+        console.log("could not load local config file", exception.message)
+    }
+}
+
+
+
+
 mongoose.connect(dbConfig.databaseURL, dbOptions).then(() => {
     app.listen(port, () => console.log(`App listening on port ${port}!`))
 })
